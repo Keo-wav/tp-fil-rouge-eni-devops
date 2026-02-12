026e56632aad21e25dca23c0f2061a9892d5a52d
keo <n.nackphouminh@gmail.com>
2026-02-12 10:29:43 +0100

    oh merde mes passwords

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  HOST: process.env.DB_HOST || 'localhost',
-  USER: process.env.DB_USER || 'root',
-  PASSWORD: process.env.DB_PASSWORD || 'rootpassword',
-  DB: process.env.DB_NAME || 'todolist_db',
-  dialect: process.env.DB_DIALECT || 'mysql',
+  HOST: process.env.DB_HOST,
+  USER: process.env.DB_USER,
+  PASSWORD: process.env.DB_PASSWORD,
+  DB: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT,
   pool: {
     max: 5,
     min: 0,
