7ee0921b2b6c65c383e004124019e9096e1ea58c
keo <n.nackphouminh@gmail.com>
2026-02-13 09:32:05 +0100

    test

--- src/server.js
+++ src/server.js
@@ -4,12 +4,9 @@ const db = require('./models');
 
 const PORT = process.env.PORT || 3000;
 
-// On lance le serveur immédiatement
-app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
-  
-  // On tente la synchro DB en arrière-plan
-  db.sequelize.sync()
-    .then(() => console.log("✅ DB Synchronized"))
-    .catch(err => console.error("❌ DB Sync failed:", err.message));
+// Synchronisation Sequelize puis lancement du serveur
+db.sequelize.sync().then(() => {
+  app.listen(PORT,'0.0.0.0', () => {
+    console.log(`✅ Server running on http://localhost:${PORT}`);
+  });
 });
